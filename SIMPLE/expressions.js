3c0115694d5bee43a1c1f58ffeb1842eddf13700
Guy Smith <gcschino@hotmail.com>
2017-06-02 10:07:09 +0100

    First commit, started making expressions as classes

---
+++ SIMPLE/expressions.js
@@ -0,0 +1,46 @@
+// SIMPLE expressions
+
+class Number {
+	constructor(value) {
+		this.value = value
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.value}>>`;
+	}
+}
+
+class Add {
+	constructor(left, right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.left} + ${this.right}>>`;
+	}
+}
+
+class Multiply {
+	constructor(left, right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		// return `<<${this.left * this.right}>>`;
+		return `<<${this.left} * ${this.right}>>`;
+	}
+}
