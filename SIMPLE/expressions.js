5fdb0e260564951ab69f3c860ca088b3a1a66cd0
Guy Smith <gcschino@hotmail.com>
2017-06-02 11:35:41 +0100

    Added variable and environment, updated reduce functions to include environment

--- SIMPLE/expressions.js
+++ SIMPLE/expressions.js
@@ -30,11 +30,11 @@ class Add {
 		return `<<${this.left} + ${this.right}>>`;
 	}
 
-	reduce() {
+	reduce(environment) {
 		if (this.left.reducible) {
-			return new Add(this.left.reduce(), this.right)
+			return new Add(this.left.reduce(environment), this.right)
 		} else if (this.right.reducible) {
-			return new Add(this.left, this.right.reduce())
+			return new Add(this.left, this.right.reduce(environment))
 		} else {
 			return new Number(this.left.value + this.right.value)
 		}
@@ -53,28 +53,167 @@ class Multiply {
 	}
 
 	toString() {
-		// return `<<${this.left * this.right}>>`;
 		return `<<${this.left} * ${this.right}>>`;
 	}
 
-	reduce() {
+	reduce(environment) {
 		if (this.left.reducible) {
-			return new Multiply(this.left.reduce(), this.right)
+			return new Multiply(this.left.reduce(environment), this.right)
 		} else if (this.right.reducible) {
-			return new Multiply(this.left, this.right.reduce())
+			return new Multiply(this.left, this.right.reduce(environment))
 		} else {
 			return new Number(this.left.value * this.right.value)
 		}
 	}
 }
 
+class Subtract {
+	constructor(left, right) {
+		this.left = left;
+		this.right = right;
+		this.reducible = true;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.left} - ${this.right}>>`;
+	}
+
+	reduce(environment) {
+		if (this.left.reducible) {
+			return new Subtract(this.left.reduce(environment), this.right)
+		} else if (this.right.reducible) {
+			return new Subtract(this.left, this.right.reduce(environment))
+		} else {
+			return new Number(this.left.value - this.right.value)
+		}
+	}
+}
+
+class Divide {
+	constructor(left, right) {
+		this.left = left;
+		this.right = right;
+		this.reducible = true;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.left} / ${this.right}>>`;
+	}
+
+	reduce(environment) {
+		if (this.left.reducible) {
+			return new Divide(this.left.reduce(environment), this.right)
+		} else if (this.right.reducible) {
+			return new Divide(this.left, this.right.reduce(environment))
+		} else {
+			return new Number(this.left.value / this.right.value)
+		}
+	}
+}
+
+
+class Boolean {
+	constructor(value) {
+		this.value = value;
+		this.reducible = false;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.value}>>`;
+	}
+}
+
+class LessThan {
+	constructor(left, right) {
+		this.left = left;
+		this.right = right;
+		this.reducible = true;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.left} < ${this.right}>>`;
+	}
+
+	reduce(environment) {
+		if (this.left.reducible) {
+			return new LessThan(this.left.reduce(environment), this.right)
+		} else if (this.right.reducible) {
+			return new LessThan(this.left, this.right.reduce(environment))
+		} else {
+			return new Boolean(this.left.value < this.right.value)
+		}
+	}
+}
+
+class GreaterThan {
+	constructor(left, right) {
+		this.left = left;
+		this.right = right;
+		this.reducible = true;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.left} > ${this.right}>>`;
+	}
+
+	reduce(environment) {
+		if (this.left.reducible) {
+			return new GreaterThan(this.left.reduce(environment), this.right)
+		} else if (this.right.reducible) {
+			return new GreaterThan(this.left, this.right.reduce(environment))
+		} else {
+			return new Boolean(this.left.value > this.right.value)
+		}
+	}
+}
+
+class Variable {
+	constructor(value) {
+		this.value = value;
+		this.reducible = true;
+	}
+
+	inspect() {
+		return this.toString();
+	}
+
+	toString() {
+		return `<<${this.value}>>`;
+	}
+
+	reduce(environment) {
+		return environment[this.value]
+	}
+}
+
 class Machine {
-	constructor(expression) {
+	constructor(expression, environment) {
 		this.expression = expression;
+		this.environment = environment;
 	}
 
 	step() {
-		this.expression = this.expression.reduce()
+		this.expression = this.expression.reduce(this.environment)
 	}
 
 	run() {
@@ -82,7 +221,7 @@ class Machine {
 			console.log(this.expression);
 			this.step();
 		}
-		console.log(expression)
+		console.log(this.expression)
 	}
 }
 
@@ -90,5 +229,11 @@ module.exports = {
 	Number,
 	Add,
 	Multiply,
+	Subtract,
+	Divide,
+	Boolean,
+	LessThan,
+	GreaterThan,
+	Variable,
 	Machine
 }
